252de67d0030d684ae5bc74c90cf6befeb54422b
drpdayan <drpdayan@mymail.mapua.edu.ph>
2024-07-15 23:23:47 +0800

    Initial Updates

--- FindLessee.py
+++ FindLessee.py
@@ -1,8 +1,13 @@
 import flet as ft
-import initiallogic as ilg  
+import initiallogic as ilg 
+from flet import *
 
 
 def main(page: ft.Page):
+  page.fonts = {
+      "RobotoSlab": "https://github.com/google/fonts/raw/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf",
+      "RobotoMono": "https://github.com/google/fonts/raw/main/apache/robotomono/RobotoMono%5Bwght%5D.ttf"
+  }
   def button_clicked(e):
     if ilg.open_connection():
       t.value = "Connection ok..."
@@ -18,9 +23,23 @@ def main(page: ft.Page):
       t.value = "NO connection..."
     page.update()
 
-  t = ft.Text(color='#7B3F00')
+  t = Text(color='#D67229',
+              weight=FontWeight.NORMAL,
+              size=25,
+              font_family="RobotoMono",
+              )
+  HeadingText = Text("Lessee Info:", color='black',
+                     weight=FontWeight.BOLD,
+                     size=40,
+                     tooltip="Home Page",
+                     font_family="RobotoSlab")
+
   tb1 = ft.TextField(label="Enter Unit Number")
-  b = ft.ElevatedButton(text="Find", on_click=button_clicked)
-  page.add(tb1, b, t)
+  b = ft.IconButton(icon=icons.SEARCH_SHARP, on_click=button_clicked)
+  page.add(
+    Row([tb1, b,]),
+    Row([HeadingText], alignment=MainAxisAlignment.CENTER),
+    Row([t], alignment=MainAxisAlignment.CENTER)
+    )
 
 ft.app(target=main)
